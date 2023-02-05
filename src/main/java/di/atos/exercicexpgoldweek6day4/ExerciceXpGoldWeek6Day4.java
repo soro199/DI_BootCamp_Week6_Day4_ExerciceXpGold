72300f07f4c74ccc2e394d8c27728e9980d2f238
seydou soro <w.soro@sectester.io>
2023-02-05 11:22:42 +0000

    ExerciceXpGold

---
+++ src/main/java/di/atos/exercicexpgoldweek6day4/ExerciceXpGoldWeek6Day4.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package di.atos.exercicexpgoldweek6day4;
+
+import di.atos.exercicexpgoldweek6day4.exercice2.PairOfDice;
+
+
+/**
+ *
+ * @author SORO
+ *@date 29/01/2023
+ */
+
+public class ExerciceXpGoldWeek6Day4 {
+
+    public static void main(String[] args) {
+        PairOfDice pdc = new  PairOfDice();
+        System.out.println(pdc.rolledDicePair());
+    }
+}
+
+
+/**
+ *
+ * @author SORO
+ *@date 29/01/2023
+ */
